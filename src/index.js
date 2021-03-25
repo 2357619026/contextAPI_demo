3f544c1370705992f7a0f3c45206e91677ef9660
程同学 <57605319+2357619026@users.noreply.github.com>
2021-03-25 18:09:56 +0800

    测试了DatePicker的组件

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import { FormDemo } from "./formDemo";
 
 import App from "./App";
-
+import { Picker_demo } from "./picker_demo";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     {/* <App /> */}
-    <FormDemo />
+    {/* <FormDemo /> */}
+    <Picker_demo />
   </StrictMode>,
   rootElement
 );
